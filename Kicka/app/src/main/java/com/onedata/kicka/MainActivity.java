733d0361c1b95346a2e3af762b5e7b9447a805b9
iamrajaguru <rajaguru2697@gmail.com>
2019-06-06 13:12:20 +0530

    First

--- Kicka/app/src/main/java/com/onedata/kicka/MainActivity.java
+++ Kicka/app/src/main/java/com/onedata/kicka/MainActivity.java
@@ -9,9 +9,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,6 +23,7 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
    TextView e1,e2;
     Button btn;
+    String url="http://192.168.1.102:8083/setuser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,37 +37,41 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Entry",Toast.LENGTH_SHORT).show();
+
                 jsonparse();
             }
         });
 
     }
     private void jsonparse(){
-        String url="https://api.myjson.com/bins/v90jj";
-        Toast.makeText(getApplicationContext(),"Entry2",Toast.LENGTH_SHORT).show();
-        JsonObjectRequest jor=new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+      /*  Toast.makeText(getApplicationContext(),"Entry2",Toast.LENGTH_SHORT).show();*/
+        JsonRequest<JSONObject> jsonRequest=new JsonRequest(Request.Method.POST, "http://192.168.1.102:8083/setuser", null,
+                new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(),"Entry",Toast.LENGTH_SHORT).show();
+                try {
+                    e1.setText(response.getString("name"));
+                    e2.setText(response.getString("password"));
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-                    try {
-                        e1.setText(response.getString("name"));
-                        e2.setText(response.getString("id"));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                /*catch(JSONException e){
-                    e.printStackTrace();*/
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
+            }},
+                            new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+                        }
+        });
+                    RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                    requestQueue.add(jsonRequest);
 
 
-    });
+
 
     }
 }
+
